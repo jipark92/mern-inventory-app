5fec12e6fa4bb737a65acc4e7e1c39ab396c5181
Ji Park <jiyongprk@gmail.com>
2022-05-18 20:38:32 -0400

    worked on GET part of backend.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const InventoryModel = require('./models/Inventory')
 
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect('mongodb+srv://norf:norf@cluster0.5zzej.mongodb.net/inventory?retryWrites=true&w=majority')
+
+app.get('/inventory', async(req,res)=>{
+    InventoryModel.find({},(err,result)=>{
+        try {
+            res.json(result)
+        } catch (err) {
+            res.send(err)
+        }
+    })
+})
+
 
 app.listen(3001, ()=>{console.log(`connected to port 3001`)})
--- server/models/Inventory.js
+++ server/models/Inventory.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose')
 
 const InvetorySchema = new mongoose.Schema({
     name:{
-        type: string,
+        type: String,
         required: true
     },
-    
+    quantity:{
+        type: Number,
+        required: true
+    },
+    detail:{
+        type: String,
+        required: true
+    }
 })
+
+const InventoryModel = mongoose.model('inventory', InvetorySchema)
+
+module.exports = InventoryModel
